2c62fda23afb032f4a77d4e87304a035aa32dd7a
Rafael Ferreira <rafamota2@hotmail.com>
2024-01-15 17:19:19 -0300

    add conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente; 
+        float saldo;
+        
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = sc.next();                
+
+        System.out.println("Por favor, digite o valor do saldo: ");
+        saldo = sc.nextFloat();
+
+        System.out.println("Olá, " + nomeCliente + "! Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo: R$" + saldo + " já está disponível para saque.");
+        
     }
 }
